ff3450d2db1f91377819e73ca022b90da6f5cdf9
sm1ke <smike143@yandex.ru>
2023-04-28 12:12:18 +0500

    feat: создать контроллеры и роуты для пользователей

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const { PORT = 3000 } = process.env;
-
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '644ad635cc9c8f4bd82e5087',
+  };
+
+  next();
+});
+
+app.use('/users', require('./routes/users'));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
