52263a5b86865406bd79c0abc13f7ce28bb512e4
sm1ke <smike143@yandex.ru>
2023-05-02 13:59:11 +0500

    feat: добавить новые поля для модели пользователей

--- models/user.js
+++ models/user.js
@@ -1,19 +1,33 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
-    required: true,
     type: String,
     minlength: 2,
     maxlength: 30,
+    default: 'Жак-Ив Кусто',
   },
   about: {
-    required: true,
     type: String,
     minlength: 2,
     maxlength: 30,
+    default: 'Исследователь',
   },
   avatar: {
+    type: String,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+  },
+  email: {
+    required: true,
+    type: String,
+    unique: true,
+    validate: {
+      validator: (v) => validator.isEmail(v),
+      message: 'Пользователь с данным Email уже существует',
+    },
+  },
+  password: {
     required: true,
     type: String,
   },
