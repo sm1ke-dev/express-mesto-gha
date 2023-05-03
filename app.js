c0d733dfcc98e31bc34cd8280fc90284fe3f7ebc
sm1ke <smike143@yandex.ru>
2023-05-03 21:52:21 +0500

    fix: test errors

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().uri(),
+    avatar: Joi.string().pattern(/https?:\/\/w?w?w?\.?\S+\.\w{2,10}\/?\S*/),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
