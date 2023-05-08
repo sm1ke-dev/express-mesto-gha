99e75370e30a16b6ebf54d57c9cf1ad5ec43f643
sm1ke <smike143@yandex.ru>
2023-05-08 11:37:14 +0500

    fix: test errors

--- controllers/users.js
+++ controllers/users.js
@@ -86,10 +86,12 @@ const login = (req, res, next) => {
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, 'secret-key', { expiresIn: '7d' });
 
-      res.cookie('jwt', token, {
-        maxAge: 3600000 * 24 * 7,
-        httpOnly: true,
-      });
+      res
+        .cookie('jwt', token, {
+          maxAge: 3600000 * 24 * 7,
+          httpOnly: true,
+        })
+        .end();
     })
     .catch(next);
 };
